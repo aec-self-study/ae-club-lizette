382656581b7b08aa954cb1bf80377ebc9857eb9a
lizette <46300917+loor12@users.noreply.github.com>
2022-10-19 18:15:46 -0500

    Create first_query.sql

---
+++ begins/first_query.sql
@@ -0,0 +1,15 @@
+with base as (
+  select customer_id
+    , name
+    , email
+    , orders.total as number_of_orders 
+    , min(orders.created_at) as first_order_at  
+  from `analytics-engineers-club.coffee_shop.customers` customers
+  left join `analytics-engineers-club.coffee_shop.orders` orders on customers.id = orders.customer_id
+
+  group by 1, 2, 3, 4
+  )
+
+select * from base
+order by 5 asc
+limit 5
